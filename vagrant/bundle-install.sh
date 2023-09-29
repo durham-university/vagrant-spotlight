16fcec678230d9ae09a899039835ace5a7e07cf6
PHILLIPS, MATTHEW E <m.e.phillips@durham.ac.uk>
2023-09-29 14:52:21 +0100

    Handle translation.rb problem better

--- vagrant/bundle-install.sh
+++ vagrant/bundle-install.sh
@@ -6,16 +6,7 @@ echo "Start of bundle-install.sh"
 cd
 if [ ! -e dur-spotlight ]; then
     echo "Spotlight bundle install"
-    rails new dur-spotlight -m https://raw.githubusercontent.com/projectblacklight/spotlight/main/template.rb
-fi
-
-# Get missing db:migrate material
-cd
-if [ ! -e dur-spotlight/db/migrate/20140128155152_create_roles.rb ]; then
-    echo "Fetching missing db/migrate files"
-    cd dur-spotlight
-    svn export --force https://github.com/projectblacklight/spotlight/branches/main/db/migrate db/migrate
-    rake db:migrate
+    SKIP_TRANSLATION=1 rails new dur-spotlight -m https://raw.githubusercontent.com/projectblacklight/spotlight/main/template.rb
 fi
 
 echo "End of bundle-install.sh"
