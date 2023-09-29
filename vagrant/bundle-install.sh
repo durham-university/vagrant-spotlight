e224c0b6c8256a452b48e7a13c563cd103603fe6
PHILLIPS, MATTHEW E <m.e.phillips@durham.ac.uk>
2023-09-29 15:11:15 +0100

    Reinstate db:migrate

--- vagrant/bundle-install.sh
+++ vagrant/bundle-install.sh
@@ -7,6 +7,8 @@ cd
 if [ ! -e dur-spotlight ]; then
     echo "Spotlight bundle install"
     SKIP_TRANSLATION=1 rails new dur-spotlight -m https://raw.githubusercontent.com/projectblacklight/spotlight/main/template.rb
+    cd dur-spotlight
+    SKIP_TRANSLATION=1 rake db:migrate
 fi
 
 echo "End of bundle-install.sh"
